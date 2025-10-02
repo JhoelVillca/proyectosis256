9eb80a1b038340042552b64ccc3d1308ce86d296
Jhoel Villca <villca.villca.jhoelmauricio@usfx.bo>
2025-10-02 10:58:08 -0400

    con coneccion a la base de datos

---
+++ app/Models/Lugares.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lugares extends Model
+{
+    protected $table = 'Lugares';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+    protected $fillable = ['NOMBRE', 'Descripcion', 
+                            'Latitud', 'Longitud', 
+                            'estado']; 
+    
+}
